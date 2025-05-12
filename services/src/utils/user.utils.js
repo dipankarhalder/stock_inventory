ba1ec5a4406dd0f23d4b34f89c1309091023b781
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-05-12 17:34:10 +0530

    completed delete

--- services/src/utils/user.utils.js
+++ services/src/utils/user.utils.js
@@ -1,11 +1,11 @@
 const User = require('../models/user.model');
-const { core } = require('../utils');
+const { notFoundItem } = require('./core.utils');
 const { msg } = require('../constant');
 
 const getUserOrRespondNotFound = async (id, res) => {
   const user = await User.findById(id).select('-password');
   if (!user) {
-    core.notFoundItem(res, msg.user.userNotFound);
+    notFoundItem(res, msg.user.userNotFound);
     return null;
   }
   return user;
