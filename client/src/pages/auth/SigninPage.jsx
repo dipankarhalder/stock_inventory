6bdbdb33ee7fd1e0a39e1a9bddb7f359caa2f883
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-05-23 16:22:51 +0530

    added toast with optimize code

--- client/src/pages/auth/SigninPage.jsx
+++ client/src/pages/auth/SigninPage.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { useContext } from "react";
+import { toastStatus } from ".././../constant";
+import { ToastContext } from "../../shared/toast/context/ToastContext";
 
 export const SigninPage = () => {
-  return <div>SigninPage</div>;
+  const { addToast } = useContext(ToastContext);
+
+  const toastMsg = {
+    type: toastStatus.SUCCESS,
+    title: "Toast form sign-in page",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <span onClick={() => addToast(toastMsg, 3000)}>click me</span>
+    </div>
+  );
 };
