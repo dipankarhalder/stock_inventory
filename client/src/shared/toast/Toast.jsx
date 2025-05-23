5770fad83c8032b45df751870277ed4e1555085e
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-05-23 17:42:04 +0530

    updated toast

--- client/src/shared/toast/Toast.jsx
+++ client/src/shared/toast/Toast.jsx
@@ -1,3 +1,5 @@
+import { Cross } from "../../icons";
+
 export const Toast = ({ show, removeToast, toastData }) => {
   const { type, heading, description } = toastData;
 
@@ -9,15 +11,7 @@ export const Toast = ({ show, removeToast, toastData }) => {
           {description && <p>{description}</p>}
         </div>
         <button onClick={() => removeToast()}>
-          <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M15 9L9 15M9 9L15 15M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <Cross />
         </button>
       </div>
     </div>
