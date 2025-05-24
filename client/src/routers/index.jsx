9b1c7edd50fbc14536023d525e463d3002bd5796
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-05-24 17:45:24 +0530

    updated other pages

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -1,14 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { paths } from "./links";
 
-import { AuthLayout } from "@/layouts/AuthLayout";
-import { AdminLayout } from "@/layouts/AdminLayout";
+import { AuthLayout } from "../layouts/AuthLayout";
+import { AdminLayout } from "../layouts/AdminLayout";
+import { ErrorPage } from "../pages/common/ErrorPage";
 
-import { ErrorPage } from "@/pages/common/ErrorPage";
-import { SigninPage } from "@/pages/auth/SigninPage";
-import { SignupPage } from "@/pages/auth/SignupPage";
+import { SigninPage } from "../pages/auth/SigninPage";
+import { SignupPage } from "../pages/auth/SignupPage";
+import { ForgotPassPage } from "../pages/auth/ForgotPassPage";
+import { CreatePassword } from "../pages/auth/CreatePassword";
 
-import { AdminDashboardPage } from "@/pages/admin/DashboardPage";
+import { AdminDashboardPage } from "../pages/admin/DashboardPage";
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +25,14 @@ export const router = createBrowserRouter([
       {
         path: paths.register,
         element: <SignupPage />,
+      },
+      {
+        path: paths.forgot,
+        element: <ForgotPassPage />,
+      },
+      {
+        path: paths.createpassword,
+        element: <CreatePassword />,
       },
     ],
   },
