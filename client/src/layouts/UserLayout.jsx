947e90a19888b543db4f0db050b87a923e0157aa
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-05-31 16:18:42 +0530

    completed routing system from frontend

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { paths } from "../routers/links";
 import { UserSidebar } from "../components/sidebar/UserSidebar";
+import { useAuthStore } from "../stores/authStore";
 
 export const UserLayout = () => {
+  const { isToken, isRole } = useAuthStore();
+
+  if (!isToken && !isRole) {
+    return <Navigate to={paths.login} />;
+  }
+
   return (
     <div className="app_admin_cover">
       <UserSidebar />
