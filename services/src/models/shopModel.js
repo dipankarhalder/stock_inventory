6582da500f60456cd9c0a2fb1e90c4961ae10c9a
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-07-10 18:19:50 +0530

    added shop and modified warehouse

---
+++ services/src/models/shopModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+/* shop schema */
+const shopSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    code: { type: String, required: true },
+    address: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    createBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('Shop', shopSchema);
